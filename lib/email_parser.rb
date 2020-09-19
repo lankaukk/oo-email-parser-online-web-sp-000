77e77e69e44effd411ffd056f9e66397b9a42d3e
McKayla Lankau <lankaukk@yahoo.com>
2020-09-19 22:44:21 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,7 +3,7 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 class EmailAddressParser 
-  attr_accessor :email_address
+  attr_accessor :name :csv_emails
   
   def initialize(emails)
   end
